a9109cf7d380478357d00f62e38c5de58f21d5d8
Nodiko Gatchava <ghachava.nodar@tbcacademy.edu.ge>
2024-11-27 19:16:33 +0400

    Continued

---
+++ HomeworkOne/src/ChapterTwoContinue.java
@@ -0,0 +1,112 @@
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class ChapterTwoContinue {
+    public static void main(String[] main) {
+        // 2.1
+        Scanner cels = new Scanner(System.in);
+//        System.out.println("How many celsius ?");
+//        double celsius = cels.nextDouble();
+//        double farenhait = 9d / 5 * celsius + 32;
+//        System.out.println(farenhait + " Farenhait");
+
+        // 2.2
+//        System.out.println("Radius = ");
+//        double radius = cels.nextDouble();
+//        System.out.println(" Length = ");
+//        double length = cels.nextDouble();
+//        double area = radius * radius * Math.PI;
+//        double volume = area * length;
+//        System.out.println(area + " is cilinder area");
+//        System.out.println(volume + " is cilinder volume");
+
+        // 2.3
+//        System.out.println("How many feet ?  ");
+//        double foot = cels.nextDouble();
+//        double meter = foot * 0.305;
+//        System.out.println(foot + " is equal to " +  meter + " meters");
+
+        // 2.5
+//        System.out.println("Subtotal: ");
+//        double subtotal = cels.nextDouble();
+//        System.out.println("gratuity: ");
+//        double gratuityRate = cels.nextDouble();
+//        double gratuity = subtotal * gratuityRate / 100;
+//        double total = subtotal + gratuity;
+//        System.out.println("The gratuity is " + gratuity + " and total is " + total);
+
+        // 2.6
+//        System.out.println("Choose the number: ");
+//        int number = cels.nextInt();
+//        if(number >= 0 && number <= 1000){
+//            int sum = 0;
+//            int z;
+//            while (number > 0) {
+//                z = number % 10;
+//                sum += z;
+//                number = number / 10;
+//            }
+//            System.out.println("Digit sum equals " + sum);
+//        }
+
+        // 2.7
+//        int minutes = cels.nextInt();
+//        int numberOfDays = minutes / (24 * 60);
+//        int year = numberOfDays / 365;
+//        System.out.println(minutes + " minutes is approximately " + year + " year and " + numberOfDays % 365 + "days");
+
+        // 2.9
+//        double v0 = cels.nextDouble();
+//        double v1 = cels.nextDouble();
+//        double t = cels.nextDouble();
+//        double avvergeAcceleration = (v1 - v0) / t;
+//        System.out.println(avvergeAcceleration);
+
+        // 2.10
+//        System.out.println("Enter quantity of water in Kilograms: ");
+//        double waterWeight = cels.nextDouble();
+//        System.out.println("Enter the initial temperature: ");
+//        double startTemp = cels.nextDouble();
+//        System.out.println("Enter the final temperature: ");
+//        double finalTemp = cels.nextDouble();
+//        double energy = waterWeight * (finalTemp - startTemp) * 4184;
+//        System.out.println("The energy needed is: " + energy);
+
+        // 2.12
+//        System.out.print("Enter Speed and Acceleration: ");
+//        double speed = cels.nextDouble();
+//        double acceleration = cels.nextDouble();
+//        double length = speed * speed / (2 * acceleration);
+//        System.out.println(length + " meter is needed");
+
+        // 2.13
+//        System.out.println("Saving: ");
+//        double money = cels.nextDouble();
+//        double interestRate = 0.00417;
+//        double q = money;
+//        double x = 0;
+//
+//
+//        for (int i = 1; i <= 6; i++){
+//              x = q * (1 + interestRate);
+//              q = money + x;
+//
+//        }
+//        System.out.println(x);
+
+        // 2.14
+//        System.out.println("Enter weight in pounds: ");
+//        double weight = cels.nextDouble();
+//        System.out.println("Enter height in inches: ");
+//        double height = cels.nextDouble();
+//
+//        double weightInKg = weight * 0.45359237;
+//        double heightInCm = height * 0.0254;
+//        double iBm = weightInKg / (heightInCm * heightInCm);
+//        System.out.println(iBm);
+
+        // 2.15
+
+    }
+}
